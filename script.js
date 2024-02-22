fd3f1716815da1f40cfd49b5ade29aff8c6dbc12
Caleb Hammer <hammer.calebr@gmail.com>
2024-02-22 11:15:27 -0500

    added function for allowed characters

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// added funtion to create password.
+function getPassword() {
+    // different characters allowed in the password
+    var uppercaseABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowercaseABC = "abcdefghijklmnopqrstuvwxyz";
+    var numeric = "0123456789";
+    var symbols = "!@#$%^&*()_-+=<>?";
+    var mulitSelect = "";
+    var randomKey = "";
+// 
+    var keylength = prompt(
+        "Your password must be between 8 to 128 characters, enter your desired length."
+    );
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -8,7 +22,6 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
