31139e9a30ed17c00b8f763618c029d2ef5b6f32
Caleb Hammer <hammer.calebr@gmail.com>
2024-02-22 12:12:14 -0500

    added prompts to .js

--- script.js
+++ script.js
@@ -3,91 +3,50 @@ var generateBtn = document.querySelector("#generate");
 // added funtion to create password.
 function getPassword() {
     // different characters allowed in the password
-    var uppercaseABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lowercaseABC = "abcdefghijklmnopqrstuvwxyz";
-    var numeric = "0123456789";
-    var symbols = "!@#$%^&*_-+=<>?";
-    var mulitSelect = "";
+    var uppercaseABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var lowercaseABC = "abcdefghijklmnopqrstuvwxyz"
+    var numeric = "0123456789"
+    var symbols = "!@#$%^&*_-+=<>?"
+    var multiSelect = "";
     var randomKey = "";
 // 
-    var keylength = prompt(
+    var keyLength = prompt(
         "Your password must be between 8 to 128 characters, enter your desired length."
     );
-}
 
 // return window for passwords not set in criteria
 if (keyLength < 8 || keyLength > 128 || isNaN(keyLength)) {
     alert("your password does not meet the criteria. Password must be between 8 to 128 characters. Please try again!");
     return;
-// added confirm option to prompts allowing user to select what types of characters are in their password
+// added prompts to better gauge what the user is looking for in their password.
 } else {
     var upperAlpha = confirm("Do you want to include UPPERCASE characters in the password? Click confirm.");
-    var lowerCase = confirm("Do you want to include lowercase characters in the password? Click confirm.");
+    var lowerAlpha = confirm("Do you want to include lowercase characters in the password? Click confirm.");
     var numericOptions = confirm("Do you want to include numbers in the password? Click confirm");
     var specialSymbols = confirm("Do you want special characters(<!$&) in the password? Click confirm");
 } 
 
 if (upperAlpha) {multiSelect += uppercaseABC;}
-if (lowerAlpha) {multiSelect += lowecaseABC;}
-if (numericOptions) {multiSelect += symbols;}
+if (lowerAlpha) {multiSelect += lowercaseABC;}
+if (numericOptions) {multiSelect += numeric;}
 if (specialSymbols) {multiSelect += symbols;}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// added loop to repeat if criteria isn't met that will end when password is created
+for (i = 0; i < keyLength; i++) {
+        randomKey += multiSelect[Math.floor(Math.random() * mulitSelect.length)]
+    }
+    return(randomKey);
+}
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  function writePassword(randomKey) {
+    if (password.length === 0) {
+        return;
+    }
+    passwordText.value = randomKey;
+  }
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function () {writePassword(getPassword()) });
